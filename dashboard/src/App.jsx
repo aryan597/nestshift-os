eaf9fe4208d087a7eebf48cc992f6514f0b353b1
dora-the-explorer <82371756+aryan597@users.noreply.github.com>
2026-04-15 22:04:06 +0100

    Readme

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -132,7 +132,6 @@ function AppContent() {
         <div className="max-w-7xl mx-auto">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/neural-map" element={<NeuralMap />} />
             <Route path="/schedule" element={<SmartSchedule />} />
             <Route path="/devices" element={<Devices />} />
             <Route path="/energy" element={<Energy />} />
